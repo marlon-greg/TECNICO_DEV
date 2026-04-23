2d56758ef38ccda28f31ece6daa59d9b3457de47
marlon-greg <marlonfanger@gmail.com>
2026-04-23 10:42:10 -0300

    inclui firebase-config no deploy

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// ⚠️ ESTE ARQUIVO NÃO VAI PARA O GITHUB (.gitignore)
+// Cada professor mantém este arquivo na sua máquina local.
+
+var FIREBASE_CONFIG = {
+  apiKey            : "AIzaSyDFspXe8sujTgc6ylI8gOgtmQ-jjd1Woq0",
+  authDomain        : "senai-dev-sistemas.firebaseapp.com",
+  projectId         : "senai-dev-sistemas",
+  storageBucket     : "senai-dev-sistemas.firebasestorage.app",
+  messagingSenderId : "682349797201",
+  appId             : "1:682349797201:web:1ed92e07c0e9907b8afd70"
+};
